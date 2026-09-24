64ad203746341039639d7151b8f41c3a5668da61
Mishkuat Sabir <mishkuatsabir11@gmail.com>
2026-09-24 12:12:51 +0200

    Adjusting small changes for the tests file

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,7 +12,7 @@ def client():
 def test_login_page_loads(client):
     response = client.get("/login")
     assert response.status_code == 200
-    assert b"Log in" in response.data
+    assert b"Log In" in response.data
 
 
 def test_signup_page_loads(client):
