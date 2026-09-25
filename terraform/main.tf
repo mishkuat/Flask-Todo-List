997ad085ddf8b73aba128520be3b6f349364f0fe
Mishkuat Sabir <mishkuatsabir11@gmail.com>
2026-09-25 11:41:38 +0200

    terraform changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "app_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["188.148.231.134/32"]
+    cidr_blocks = ["0.0.0.0/0"]
     description = "SSH from my IP"
   }
 
