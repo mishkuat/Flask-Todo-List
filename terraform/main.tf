ee832434e19f29d4a996269d9dad475625be0a9e
Mishkuat Sabir <mishkuatsabir11@gmail.com>
2026-09-25 11:44:28 +0200

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "aws_security_group" "app_sg" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "SSH from my IP"
+    description = "SSH from my IP address"
   }
 
   ingress {
